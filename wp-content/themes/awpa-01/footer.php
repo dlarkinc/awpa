e5a48df64d0ac7259dfa0006b95775ddd6138d15
larkinc <larkin.cunningham@cit.ie>
2015-02-09 19:32:13 +0000

    Week 1 version of theme

---
+++ wp-content/themes/awpa-01/footer.php
@@ -0,0 +1,6 @@
+        <div id="footer">
+                <p>&copy; Larkin Cunningham, 2014</p>
+        </div>
+    </div>
+</body>
+</html>
---
+++ wp-content/themes/awpa-01/header.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <title>AWPA theme</title>
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+</head>
+
+<body>
+<div id="wrapper"/>
+<!-- wraps entire body, ending in footer -->
+<div id="header">
+ <h1>Welcome to Advanced Web Publishing Apps</h1>
+</div>
